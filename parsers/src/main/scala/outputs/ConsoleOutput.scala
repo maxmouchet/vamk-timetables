c17a869c5b63d20d88603f5d3f73172b3733eb6f
Maxime Mouchet <max@maxmouchet.com>
2014-02-16 08:34:13 +0200

    Add ConsoleOutput description

--- parsers/src/main/scala/outputs/ConsoleOutput.scala
+++ parsers/src/main/scala/outputs/ConsoleOutput.scala
@@ -3,6 +3,7 @@ package outputs
 import models.Schedule
 import outputs.base.Output
 
+/** Print all the schedules to stdout. */
 class ConsoleOutput extends Output {
 
   def execute(args: Array[String], schedules: Array[Schedule]) = {
